b20f2e3c038ff18286af8e120caffb732b3b1f62
Luis Leonardo <47133311+Luis-Leonardo@users.noreply.github.com>
2020-11-27 21:40:06 -0600

    Agregado Sesion Usuario

--- app/src/main/java/com/example/cexpress_vendedor/LoginActivity.java
+++ app/src/main/java/com/example/cexpress_vendedor/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.cexpress_vendedor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -14,14 +15,19 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     EditText txtUsuario, txtPasswd;
     Button btnIngresar, btnRegistrar;
@@ -29,10 +35,16 @@ public class LoginActivity extends AppCompatActivity {
     boolean user=false;
     boolean pass=false;
 
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //aqui se llama a la funcion para verificar antes que el resto
+        verificarSesion();
 
         txtUsuario = findViewById(R.id.Usuario);
         txtPasswd = findViewById(R.id.Password);
@@ -41,6 +53,8 @@ public class LoginActivity extends AppCompatActivity {
 
         impUsuario = findViewById(R.id.impUsuario);
         impPasswd = findViewById(R.id.impPassword);
+
+        request = Volley.newRequestQueue(this);
 
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +86,7 @@ public class LoginActivity extends AppCompatActivity {
                 //Validaciones contraseña
                 if (txtPasswd.getText().toString().isEmpty()) {
                     impPasswd.setError("Campo de contraseña está vacío");
-                    user = false;
+                    pass = false;
                 } else {
                     impPasswd.setError(null);
                     pass = true;
@@ -89,32 +103,52 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     void  login(final String correo, final String password) {
-        String URL = "https://appsmoviles2020.000webhostapp.com/vendedor/login.php";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (!response.isEmpty()) {
-                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(i);
-                } else {
-                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-                }
+        String URL = "https://appsmoviles2020.000webhostapp.com/vendedor/login.php?correo="+correo+"&password="+password;
+        URL = URL.replaceAll("@", "%40");
+
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, this, this);
+        request.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        error.printStackTrace();
+        Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        try {
+            JSONArray jsonArray = response.getJSONArray("Vendedor");
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            if(jsonObject.getString("idVendedor").equals("0")) {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_LONG).show();
+            } else {
+                generarSesion(jsonObject.getString("idVendedor"));
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(LoginActivity.this, "Error con el servidor. Inténtelo más tarde", Toast.LENGTH_SHORT).show();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> parametros = new HashMap<>();
-                parametros.put("correo", correo);
-                parametros.put("password", password);
-                return  parametros;
-            }
-        };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //con esta función se guarda el id del usuario en caso de que el login sea correcto
+    void generarSesion(String idVendedor) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Sesion", MODE_PRIVATE);
+        sharedPreferences.edit().putInt("id", Integer.valueOf(idVendedor)).apply();
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    //con esta funcion se verifica si hay una sesion iniciada
+    void verificarSesion() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Sesion", MODE_PRIVATE);
+        int id = sharedPreferences.getInt("id", 0);
+
+        if(id!=0) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/cexpress_vendedor/MainActivity.java
+++ app/src/main/java/com/example/cexpress_vendedor/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cexpress_vendedor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,9 +26,7 @@ public class MainActivity extends AppCompatActivity {
         btnSalir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(i);
-                finish();
+                finalizarSesion();
             }
         });
 
@@ -54,5 +53,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    //con esta función se modifica el valor del id para cerrar la sesión
+    void finalizarSesion() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Sesion", MODE_PRIVATE);
+        sharedPreferences.edit().putInt("id", Integer.valueOf(0)).apply();
+
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
