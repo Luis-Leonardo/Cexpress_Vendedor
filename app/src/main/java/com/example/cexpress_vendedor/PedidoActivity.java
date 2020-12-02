ab2606aee3e628d6a4a6076b4424e38e1aa192d2
Luis Leonardo <47133311+Luis-Leonardo@users.noreply.github.com>
2020-12-02 01:17:37 -0600

    Agregado Cambio Estado Pedido

--- app/src/main/java/com/example/cexpress_vendedor/PedidoActivity.java
+++ app/src/main/java/com/example/cexpress_vendedor/PedidoActivity.java
@@ -2,18 +2,24 @@ package com.example.cexpress_vendedor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -21,11 +27,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PedidoActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
     TextView txtViewIdPedidoDetalle, txtViewFechaPedidoDetalle, txtViewNegocioPedidoDetalle, txtViewEstadoPedidoDetalle,
             txtViewCompradorPedidoDetalle, txtViewOrden;
     ImageButton imgBtnRegresar;
+    Button btnPedidoListo;
 
     int idPedido;
     String fecha, negocio, estado, comprador;
@@ -56,6 +65,7 @@ public class PedidoActivity extends AppCompatActivity implements Response.Listen
         txtViewCompradorPedidoDetalle = findViewById(R.id.txtViewCompradorPedidoDetalle);
         txtViewOrden = findViewById(R.id.txtViewOrden);
         imgBtnRegresar = findViewById(R.id.imgBtnRegresar);
+        btnPedidoListo = findViewById(R.id.btnPedidoListo);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -66,6 +76,7 @@ public class PedidoActivity extends AppCompatActivity implements Response.Listen
         if(estado.equals("En preparación")) {
             txtViewEstadoPedidoDetalle.setTextColor(0xFFFF9800);
             txtViewEstadoPedidoDetalle.setText("Preparación");
+            btnPedidoListo.setVisibility(View.VISIBLE   );
         } else if (estado.equals("En camino")) {
             txtViewEstadoPedidoDetalle.setTextColor(0xE9ECAE18);
             txtViewEstadoPedidoDetalle.setText(estado);
@@ -85,6 +96,49 @@ public class PedidoActivity extends AppCompatActivity implements Response.Listen
         request = Volley.newRequestQueue(this);
 
         cargarDatos();
+
+        btnPedidoListo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(PedidoActivity.this);
+                dialog.setMessage("Esta acción no es reversible.\n¿Quiere marcar el pedido como listo para recogerlo?").setPositiveButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String URL = "https://appsmoviles2020.000webhostapp.com/vendedor/pedidoListo.php";
+
+                        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                System.out.println(response);
+                                Toast.makeText(PedidoActivity.this, "El pedido está listo para ser recogido", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(PedidoActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }){
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> params = new HashMap<>();
+                                params.put("idPedido", String.valueOf(idPedido));
+
+                                return params;
+                            }
+                        };
+                        request.add(stringRequest);
+                    }
+                });
+                dialog.create();
+                dialog.show();
+            }
+        });
     }
 
     void cargarDatos() {
